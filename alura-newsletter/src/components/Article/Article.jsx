407596402b55e2ffb1056cfe2b4d45bd823a0ba5
Fabiano <fabiano.ssantos79@gmail.com>
2023-08-23 01:32:04 -0300

    update

--- alura-newsletter/src/components/Article/Article.jsx
+++ alura-newsletter/src/components/Article/Article.jsx
@@ -1,7 +1,19 @@
 const Article = ({ title, text, tags, image, alt }) => {
     return (
-        <div className="p-5 bg-gray-200 dark:bg-gray-500 sm:rounded-xl sm:shadow-lg flex flex-col items-center">
+        <div className="p-5 bg-gray-200 dark:bg-gray-500 sm:rounded-xl sm:shadow-lg flex flex-col items-center gap-2">
+
             <h3 className="text-xl font-bold text-gray-700 dark:text-gray-100">{title}</h3>
+
+            <div className="w-full justify-end gap-4 hidden md:flex">
+                {
+                    tags.map((tag, index) => <span key={index} className="bg-gray-500 py-1 px-2 rounded-full text-xs text-white font-semibold dark:bg-gray-200 dark:text-gray-700">{tag}</span>)
+
+                }
+            </div>
+
+            {image && <img src={image} className="py-2" />}
+            {image && alt && <span className="sr-only">{alt}</span>}
+
             <div className="grid gap-4">
                 {
                     text.map((content, index) => <p className="text-gray-700 dark:text-gray-300" key={index}>{content}</p>)
--- alura-newsletter/src/components/ArticleList/ArticleList.jsx
+++ alura-newsletter/src/components/ArticleList/ArticleList.jsx
@@ -3,7 +3,7 @@ import Article from '../Article/Article';
 
 const ArticleList = () => {
     return (
-        <div className='mt-5 grid gap-5 max-w-2xl m-auto sm:grid-cols-2 lg:grid-cols-3 sm:mt-0 lg:max-w-[1000px]'>
+        <div className='my-5 grid gap-5 max-w-2xl m-auto sm:grid-cols-2 lg:grid-cols-3 sm:mt-0 lg:max-w-[1000px]'>
             {data.map((props, index) => <Article key={index} {...props} />)
             }
         </div>
